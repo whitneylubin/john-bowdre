030a825084c7dd977873d4ebdc21eee1e8505f75
Whitney Lubin <whitneylubin@icloud.com>
2018-12-16 15:50:03 -0500

    moved container div

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,9 @@ import React from "react";
 const Header = () => (
   <header className="masthead d-flex">
     <div className="container text-center my-auto">
-      <h1 className="mb-1">Stylish Portfolio</h1>
+      <h1 className="mb-1">John Bowdre</h1>
       <h3 className="mb-5">
-        <em>A Free Bootstrap Theme by Start Bootstrap</em>
+        <em>Technology | China Trade</em>
       </h3>
       <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">
         Find Out More
