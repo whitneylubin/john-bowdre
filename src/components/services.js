18a8d6588a262d18f8e7e8c4154d13df4c34f15e
Whitney Lubin <whitneylubin@icloud.com>
2018-12-17 13:00:20 -0500

    edited services component

--- src/components/services.js
+++ src/components/services.js
@@ -32,7 +32,7 @@ const ServicesSections = () => (
       <div className="container">
         <div className="content-section-heading">
           <h3 className="text-secondary mb-0">Services</h3>
-          <h2 className="mb-5">What We Offer</h2>
+          <h2 className="mb-5">My Services</h2>
         </div>
         <div className="row">
           {ServicesOffered.map((service, index) => (
